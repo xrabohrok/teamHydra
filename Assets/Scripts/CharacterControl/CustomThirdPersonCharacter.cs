1ffc18214c30e318e5cbdfdef88b7965212c03e7
Mr. Black <westin007@gmail.com>
2016-07-05 22:30:33 -0500

    fixed a slight bug with the movement code, didnt consider we are controlling ghosts - Mr.Black

--- Assets/Scripts/CharacterControl/CustomThirdPersonCharacter.cs
+++ Assets/Scripts/CharacterControl/CustomThirdPersonCharacter.cs
@@ -69,9 +69,10 @@ namespace Characters.CustomThirdPerson
 			// helper to visualise the ground check ray in the scene view
 			Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
 #endif
-			// 0.1f is a small offset to start the ray from inside the character
-			// it is also good to note that the transform position in the sample assets is at the base of the character
-			if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
+            // 0.1f is a small offset to start the ray from inside the character
+            // it is also good to note that the transform position in the sample assets is at the base of the character
+	        int filter = 1 << LayerMask.NameToLayer("Ground");
+            if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance, filter))
 			{
 				m_GroundNormal = hitInfo.normal;
 				m_IsGrounded = true;
